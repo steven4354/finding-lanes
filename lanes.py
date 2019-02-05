5d296a1b57b7025d10cbf5b14d9eddc470d37206
Steven Li <steven4354@gmail.com>
2019-02-04 23:21:52 -0800

    polygon pic

--- lanes.py
+++ lanes.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # TODO: place in section comments
 
-def simplifyImg(lane_image):
+def simplify_img(lane_image):
     # change the image to gray for faster processing
     # reasoning: 1 value in each pixel array rather than 3 (RGB)
     gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
@@ -23,18 +23,34 @@ def simplifyImg(lane_image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def remove_all_outside_region_of_car_lane(image):
+    # gets the bottom of the image (max # - just show image to see)
+    height = image.shape[0]
+    # array of triangles
+    # each triangle array [point A, point B, point C]
+    # the non-height numbers were just eye-balled using the plot
+    polygons = np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    # blacken everything in image
+    mask = np.zeros_like(image)
+    # take black image and place a polygon inside, fill everything in the poly with color 255
+    cv2.fillPoly(mask, polygons, 255)
+    return mask
+
 # reads the image & returns a multidimension array
 # np.copy is just a clone of it so we don't edit the original
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-canny = simplifyImg(lane_image)
+canny = simplify_img(lane_image)
 
-
+# plops a black image with a polygon
+lane = remove_all_outside_region_of_car_lane(canny)
 # shows the image when ran (opens in a new window)
 # @waitKey sets the opened image to stay open until x is pressed
 # cv2.imshow('result', canny)
 # cv2.waitKey(0)
 
 # shows the image with a graph
-plt.imshow(canny)
+plt.imshow(lane)
 plt.show()
